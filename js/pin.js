757cb579ebc5d16a22728d16d5972c9dfa9c8905
AnnaShant <anna.lunatic.t@gmail.com>
2020-02-24 17:11:57 +0500

    Задание 5

---
+++ js/pin.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+  var PinData = {
+    WIDTH_PIN: 50,
+    HEIGHT_PIN: 70
+  };
+
+  var mapPinsElement = document.querySelector('.map__pins');
+  var pinTemplateElement = document.querySelector('#pin').content;
+  var pinElement = pinTemplateElement.querySelector('.map__pin');
+
+  var createElementPin = function (num) {
+    var element = pinElement.cloneNode(true);
+
+    element.querySelector('.map__pin').style.left = num.location.x - PinData.WIDTH_PIN / 2 + 'px';
+    element.querySelector('.map__pin').style.top = num.location.y - PinData.HEIGHT_PIN + 'px';
+    element.querySelector('img').src = num.author.avatar.src;
+    element.querySelector('img').alt = num.offer.title;
+
+    return element;
+  };
+
+  window.pin = {
+    renderAd: function () {
+      var fragment = document.createDocumentFragment();
+      window.card();
+      for (var j = 0; j < window.AMOUNT_QUARTERS; j++) {
+        fragment.appendChild(createElementPin(j));
+      }
+      mapPinsElement.appendChild(fragment);
+    }
+  };
+})();
